2d5aa4b4e2035f170ca2e52b5d6f653456f447c0
Sergey Lukjanov <slukjanov@mirantis.com>
2013-03-07 17:35:41 +0400

    test job has been disabled

--- eho/server/scheduler.py
+++ eho/server/scheduler.py
@@ -1,14 +1,7 @@
-import logging
 import eventlet
 
 POOL = eventlet.GreenPool()
 
 
-def test_job_fun(idx):
-    logging.info("Test periodic job, iteration #%s", idx)
-    eventlet.spawn_after(5, test_job_fun, idx + 1)
-
-
 def setup_scheduler(app):
     app.pool = POOL
-    eventlet.spawn(test_job_fun, 0)
