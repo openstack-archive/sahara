c9e1f7296ad3c3bc688f221c4c301b3d05edfddc
Nikita Konovalov <nkonovalov@mirantis.com>
2013-07-22 10:55:37 +0400

    Licence header added to tools/get_auth_token.py
    Imports in this file refactored also
    
    Fixes: bug #1203408
    
    Change-Id: Ibb4aaa271d3e0738c0c1d114efdefcba856fb190

--- tools/get_auth_token.py
+++ tools/get_auth_token.py
@@ -1,7 +1,23 @@
-from keystoneclient.v2_0 import Client as keystone_client
+# Copyright (c) 2013 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-from oslo.config import cfg
 import sys
+
+from keystoneclient.v2_0 import Client as keystone_client
+from oslo.config import cfg
 
 
 possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
