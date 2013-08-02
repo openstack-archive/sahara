cd27739bf172fccf0857f49ce5314a7b4e98c748
Jon Maron <jmaron@hortonworks.com>
2013-08-02 14:25:40 -0400

    Added validation checks to HDP plugin
    Included checks for namenode, jobtracker, ambari server, and ambari agent
    
    Fixes: bug #1207850
    
    Change-Id: I76719430d75cb2c3b00c4dd63190659ca8046efb

--- savanna/plugins/vanilla/scaling.py
+++ savanna/plugins/vanilla/scaling.py
@@ -16,8 +16,8 @@
 import os
 
 from savanna import context
+from savanna.plugins.general import utils
 from savanna.plugins.vanilla import run_scripts as run
-from savanna.plugins.vanilla import utils
 from savanna.utils import remote
 
 
