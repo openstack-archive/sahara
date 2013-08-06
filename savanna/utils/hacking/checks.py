9017a6db3619837d87e5d53de1b894f7a0ac581c
Sergey Lukjanov <slukjanov@mirantis.com>
2013-08-06 17:10:01 +0400

    Add check S361 for imports of savanna.db module
    It should be possible to import savanna.db module only from conductor
    and tests.
    
    Change-Id: I5992245b12fa7fc1246a7565839e6dd222a8bf8a

--- savanna/utils/hacking/checks.py
+++ savanna/utils/hacking/checks.py
@@ -14,5 +14,40 @@
 # limitations under the License.
 
 
+def _starts_with_any(line, *prefixes):
+    for prefix in prefixes:
+        if line.startswith(prefix):
+            return True
+    return False
+
+
+def _any_in(line, *sublines):
+    for subline in sublines:
+        if subline in line:
+            return True
+    return False
+
+
+def import_db_only_in_conductor(logical_line, filename):
+    """Check that db calls are only in conductor module and in tests.
+
+    S361
+    """
+    if _any_in(filename,
+               "savanna/conductor",
+               "savanna/tests",
+               "savanna/db",
+               # TODO(slukjanov): remove this when we'll use new db abstraction
+               "savanna/db_new"):
+        return
+
+    if _starts_with_any(logical_line,
+                        "from savanna import db",
+                        "from savanna.db",
+                        "import savanna.db"):
+        yield (0, "S361: savanna.db import only allowed in "
+                  "savanna/conductor/*")
+
+
 def factory(register):
-    pass
+    register(import_db_only_in_conductor)
