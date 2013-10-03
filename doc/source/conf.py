43a67fe05fcb6f3a70293996c80671884e969b21
Sergey Lukjanov <slukjanov@mirantis.com>
2013-10-03 13:22:54 +0400

    Replace copy-pasted sphinx theme with oslo.sphinx
    Change-Id: I3c78753ea5d800b67853af96cc155707cdbf7755

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,7 +35,7 @@ sys.path.append(os.path.abspath('../bin'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage',
-              'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'sphinxcontrib.httpdomain']
+              'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'sphinxcontrib.httpdomain', 'oslo.sphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -102,8 +102,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme_path = ['.']
-html_theme = '_theme'
+#html_theme_path = ['.']
+#html_theme = '_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
