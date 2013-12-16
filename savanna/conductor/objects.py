d33f678a0929c54599307fdb3005d8f3fd9fab9e
Sergey Reshetnyak <sreshetniak@mirantis.com>
2013-12-16 17:41:50 +0400

    Add generating new keypair to hadoop user in vanilla plugin
    * Add extra field to cluster object. This field need to storage plugin
    * specific data
    
    Fixes bug: #1261038
    
    Change-Id: Ibf96bc888843184aba2c9d1bdbd005bd3e5a3584

--- savanna/conductor/objects.py
+++ savanna/conductor/objects.py
@@ -52,6 +52,7 @@ class Cluster(object):
     status
     status_description
     info
+    extra
     node_groups - list of NodeGroup objects
     cluster_template_id
     cluster_template - ClusterTemplate object
