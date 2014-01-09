4fa4b97f6d3be71af5e103acb04d61cc00b688a9
Andrew Lazarev <alazarev@mirantis.com>
2014-01-09 15:51:25 -0800

    Fixed cluster template with no nodegroups creation
    Fixes: bug #1265609
    
    Change-Id: I25dafd0439ab743cc5bb6f0c36d212c7cc3d1c83

--- savanna/conductor/manager.py
+++ savanna/conductor/manager.py
@@ -72,7 +72,7 @@ class ConductorManager(db_base.Base):
     def _populate_node_groups(self, context, cluster):
         node_groups = cluster.get('node_groups')
         if not node_groups:
-            return
+            return []
 
         populated_node_groups = []
         for node_group in node_groups:
