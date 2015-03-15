aa19615e067610e9db01a3d007e8a366ebf459fd
Artem Osadchyi <aosadchiy@maprtech.com>
2015-03-15 20:33:24 +0200

    Install MySQL JDBC driver along with client
    Oozie can't start properly because of absence of MySQL JDBC driver in
    its lib/ directory. The jar is copied there from /usr/share/java/mysql-
    connector-java.jar during Oozie install. Driver is provided by
    libmysql-java on Ubuntu and mysql-connector-java on RedHat and SLES
    
    Change-Id: I75d70eea82ec136f49770431a1d0a872717263e1
    Closes-Bug: #1432633

--- sahara/plugins/mapr/resources/install_mysql_client.sh
+++ sahara/plugins/mapr/resources/install_mysql_client.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ $1 == *"Ubuntu"* ]]; then
-    sudo apt-get install --force-yes -y mysql-client
+    sudo apt-get install --force-yes -y mysql-client libmysql-java
 elif [[ $1 == *"CentOS"* ]] || [[ $1 == *"Red Hat Enterprise Linux"* ]]; then
-    sudo yum install -y mysql
+    sudo yum install -y mysql mysql-connector-java
 elif [[ $1 == *"SUSE"* ]]; then
-    sudo zypper install mysql-community-server-client
+    sudo zypper install mysql-community-server-client mysql-connector-java
 else
     echo "Unknown distribution"
     exit 1
