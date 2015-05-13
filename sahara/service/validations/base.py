33874f03315f40ad8fe7889d552527a51cc7fcdd
Andrey Pavlov <apavlov@mirantis.com>
2015-05-13 18:37:18 +0300

    Fixed bug with volume type validation
    check_volume_type_exists method in service.validations.base
    raised error even if volume type was valid.
    
    Change-Id: I587c416fbaacd36a48d99f9a629e5a213dbfcfe1
    Closes-bug: #1454753

--- sahara/service/validations/base.py
+++ sahara/service/validations/base.py
@@ -239,7 +239,7 @@ def check_volume_availability_zone_exist(az):
 def check_volume_type_exists(volume_type):
     volume_types = cinder.client().volume_types.list(search_opts={'name':
                                                                   volume_type})
-    if len(volume_types) == 1 and volume_types[0] == volume_type:
+    if len(volume_types) == 1 and volume_types[0].name == volume_type:
         return
     raise ex.NotFoundException(volume_type, _("Volume type '%s' not found"))
 
