6ceec4e3a48059ea8b281c1e125811a7803fa2fa
Andrew Lazarev <alazarev@mirantis.com>
2015-06-02 12:13:54 -0700

    Switched from all stacks polling to filtered list
    There is no need to get all stacks to get a specific stack.
    
    Change-Id: I67142204308ec61af4bcc9bee8f607e1fcd419c5
    Related-Bug: #1414472
    Closes-Bug: #1461212

--- sahara/utils/openstack/heat.py
+++ sahara/utils/openstack/heat.py
@@ -47,11 +47,13 @@ def client():
                               insecure=CONF.heat.api_insecure)
 
 
-def get_stack(stack_name):
-    heat = client()
-    for stack in base.execute_with_retries(heat.stacks.list):
-        if stack.stack_name == stack_name:
-            return stack
+def get_stack(stack_name, raise_on_missing=True):
+    for stack in base.execute_with_retries(
+            client().stacks.list, filters={'name': stack_name}):
+        return stack
+
+    if not raise_on_missing:
+        return None
 
     raise ex.NotFoundException(_('Failed to find stack %(stack)s')
                                % {'stack': stack_name})
