6c12c85d067f3a81055280ba37f056092c724a6b
Jaxon Wang <jiexingx.wang@intel.com>
2015-12-18 18:38:13 +0800

    Compare node groups in CDH plugin IMPALA validation
    Because instances are not initialized during validation, compare node
    groups instead. Using node group id beacuse NodeGroupReasouce is not
    hashable for python set.
    
    Change-Id: I3dd01c00d1efdaffdea1000913e4f650db72ff3a
    Closes-bug: #1527533

--- sahara/plugins/cdh/v5_3_0/validation.py
+++ sahara/plugins/cdh/v5_3_0/validation.py
@@ -205,9 +205,12 @@ def validate_cluster_creating(cluster):
         raise ex.InvalidComponentCountException('IMPALA_STATESTORE',
                                                 _('0 or 1'), iss_count)
     if ics_count == 1:
-        datanodes = set(u.get_instances(cluster, "HDFS_DATANODE"))
-        impalads = set(u.get_instances(cluster, "IMPALAD"))
-        if len(datanodes ^ impalads) > 0:
+        datanode_ng = u.get_node_groups(cluster, "HDFS_DATANODE")
+        impalad_ng = u.get_node_groups(cluster, "IMPALAD")
+        datanodes = set(ng.id for ng in datanode_ng)
+        impalads = set(ng.id for ng in impalad_ng)
+
+        if datanodes != impalads:
             raise ex.InvalidClusterTopology(
                 _("IMPALAD must be installed on every HDFS_DATANODE"))
 
