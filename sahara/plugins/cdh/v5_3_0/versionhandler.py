8af60d77fc4374a6d0f9bae2d790deb3856f574d
Jaxon Wang <jiexingx.wang@intel.com>
2015-12-24 14:11:59 +0800

    CDH plugin versionhandler refactoring
    Changes:
    * Create class BaseVersionHandler in abstractversionhandler.py.
    * Move common part into BaseVersionHandler.
    * Let real VersionHandlers in each version inherit from the base.
    
    Partially Implements: blueprint cdh-plugin-refactoring
    
    Change-Id: I1b3d2dc124e6a11f1fbb932e945f0d3256d83ba8

--- sahara/plugins/cdh/v5_3_0/versionhandler.py
+++ sahara/plugins/cdh/v5_3_0/versionhandler.py
@@ -14,26 +14,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from sahara import conductor
-from sahara import context
 from sahara.plugins.cdh import abstractversionhandler as avm
-from sahara.plugins.cdh.v5_3_0 import cloudera_utils as cu
-from sahara.plugins.cdh.v5_3_0 import config_helper as c_helper
-from sahara.plugins.cdh.v5_3_0 import deploy as dp
+from sahara.plugins.cdh.v5_3_0 import cloudera_utils
+from sahara.plugins.cdh.v5_3_0 import config_helper
+from sahara.plugins.cdh.v5_3_0 import deploy
 from sahara.plugins.cdh.v5_3_0 import edp_engine
-from sahara.plugins.cdh.v5_3_0 import plugin_utils as pu
+from sahara.plugins.cdh.v5_3_0 import plugin_utils
 from sahara.plugins.cdh.v5_3_0 import validation
 
-conductor = conductor.API
-CU = cu.ClouderaUtilsV530()
-PU = pu.PluginUtilsV530()
-vl = validation.ValidatorV530
 
+class VersionHandler(avm.BaseVersionHandler):
 
-class VersionHandler(avm.AbstractVersionHandler):
-
-    def get_plugin_configs(self):
-        return c_helper.get_plugin_configs()
+    def __init__(self):
+        super(VersionHandler, self).__init__()
+        self.config_helper = config_helper
+        self.cloudera_utils = cloudera_utils.ClouderaUtilsV530()
+        self.plugin_utils = plugin_utils.PluginUtilsV530()
+        self.deploy = deploy
+        self.edp_engine = edp_engine
+        self.validation = validation.ValidatorV530()
 
     def get_node_processes(self):
         return {
@@ -58,58 +57,15 @@ class VersionHandler(avm.AbstractVersionHandler):
             "HDFS_GATEWAY": []
         }
 
-    def validate(self, cluster):
-        vl.validate_cluster_creating(cluster)
-
-    def configure_cluster(self, cluster):
-        dp.configure_cluster(cluster)
-        conductor.cluster_update(
-            context.ctx(), cluster, {
-                'info': CU.get_cloudera_manager_info(cluster)})
-
-    def start_cluster(self, cluster):
-        dp.start_cluster(cluster)
-
-        self._set_cluster_info(cluster)
-
-    def decommission_nodes(self, cluster, instances):
-        dp.decommission_cluster(cluster, instances)
-
-    def validate_scaling(self, cluster, existing, additional):
-        vl.validate_existing_ng_scaling(cluster, existing)
-        vl.validate_additional_ng_scaling(cluster, additional)
-
-    def scale_cluster(self, cluster, instances):
-        dp.scale_cluster(cluster, instances)
-
-    def _set_cluster_info(self, cluster):
-        info = CU.get_cloudera_manager_info(cluster)
-
-        hue = CU.pu.get_hue(cluster)
-        if hue:
-            info['Hue Dashboard'] = {
-                'Web UI': 'http://%s:8888' % hue.management_ip
-            }
-
-        ctx = context.ctx()
-        conductor.cluster_update(ctx, cluster, {'info': info})
-
     def get_edp_engine(self, cluster, job_type):
-        if job_type in edp_engine.EdpOozieEngine.get_supported_job_types():
-            return edp_engine.EdpOozieEngine(cluster)
-        if job_type in edp_engine.EdpSparkEngine.get_supported_job_types():
-            return edp_engine.EdpSparkEngine(cluster)
+        oozie_type = self.edp_engine.EdpOozieEngine.get_supported_job_types()
+        spark_type = self.edp_engine.EdpSparkEngine.get_supported_job_types()
+        if job_type in oozie_type:
+            return self.edp_engine.EdpOozieEngine(cluster)
+        if job_type in spark_type:
+            return self.edp_engine.EdpSparkEngine(cluster)
         return None
 
     def get_edp_job_types(self):
-        return (edp_engine.EdpOozieEngine.get_supported_job_types() +
-                edp_engine.EdpSparkEngine.get_supported_job_types())
-
-    def get_edp_config_hints(self, job_type):
-        return edp_engine.EdpOozieEngine.get_possible_job_config(job_type)
-
-    def get_open_ports(self, node_group):
-        return dp.get_open_ports(node_group)
-
-    def recommend_configs(self, cluster, scaling):
-        PU.recommend_configs(cluster, self.get_plugin_configs(), scaling)
+        return (self.edp_engine.EdpOozieEngine.get_supported_job_types() +
+                self.edp_engine.EdpSparkEngine.get_supported_job_types())
