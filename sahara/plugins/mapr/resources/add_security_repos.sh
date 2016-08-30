06e18d80378c14025228f6069835cd091b37a8ad
Grigoriy Rozhkov <groghkov@maprtech.com>
2016-08-30 13:13:03 +0300

    Fix invalid security repo
    Change-Id: I0365b7430ff17348fa2e0a4b8a3476b149a7ccca
    Closes-Bug: #1618397
    (cherry picked from commit 1400bbeba7d7ac8854c93f92cbd3ed5ff870c558)

--- sahara/plugins/mapr/resources/add_security_repos.sh
+++ sahara/plugins/mapr/resources/add_security_repos.sh
@@ -2,6 +2,5 @@
 
 cat >> /etc/apt/sources.list.d/security_repo.list <<- EOF
 deb http://security.ubuntu.com/ubuntu precise-security main
-deb http://security.ubuntu.com/ubuntu lucid-security main
 EOF
 apt-get update
