f254ad73cbbe480ae158162f9f562454c3318fde
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2016-10-12 13:18:09 +0200

    Replace 'sudo pip install' with pip_install
    Configuration of sahara in devstack behind a proxy fails because the
    installation of "tooz[zookeeker]" does not work.
    
    This is because sudo does not preserve environment variables, so pip
    does not use the configured proxy. Using 'pip_install' instead of
    'sudo pip install' should fix this, since 'pip_install' ensures that
    the environment is preserved.
    
    Change-Id: I78d483e86c586846f92bdc7118d495efe7e6df17
    Closes-Bug: #1632673

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -157,7 +157,7 @@ function configure_sahara {
         # Enable distributed periodic tasks
         iniset $SAHARA_CONF_FILE DEFAULT periodic_coordinator_backend_url\
             $SAHARA_PERIODIC_COORDINATOR_URL
-        sudo pip install tooz[zookeeper]
+        pip_install tooz[zookeeper]
     fi
 
     recreate_database sahara
