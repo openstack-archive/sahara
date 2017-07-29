a76a9011df7cb8385315255dc98da5322e7a2bc1
zhuli <zhuli27@huawei.com>
2017-07-29 11:42:13 -0400

    Fix error during node group template update
    this is because an optional key is referenced in calling validation
    function which results in KeyError when key doesn't exist, and it
    does not ever actually get used.
    
    Change-Id: I12be69b5caf81289c2b8070fab8d2083709d6cf7
    Closes-Bug: #1672516

--- sahara/service/validations/base.py
+++ sahara/service/validations/base.py
@@ -165,7 +165,7 @@ def check_node_group_basic_fields(plugin_name, hadoop_version, ng,
                 _("You must specify a volumes_size parameter"))
 
     if ng.get('floating_ip_pool'):
-        check_floatingip_pool_exists(ng['name'], ng['floating_ip_pool'])
+        check_floatingip_pool_exists(ng['floating_ip_pool'])
 
     if ng.get('security_groups'):
         check_security_groups_exist(ng['security_groups'])
@@ -194,7 +194,7 @@ def check_security_groups_exist(security_groups):
                 sg, _("Security group '%s' not found"))
 
 
-def check_floatingip_pool_exists(ng_name, pool_id):
+def check_floatingip_pool_exists(pool_id):
     network = None
     if CONF.use_neutron:
         network = neutron.get_network(pool_id)
