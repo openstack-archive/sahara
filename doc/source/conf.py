0e6ab1de37011a90b3cfb2895db72bb2aa137766
wangqi <wang.qi@99cloud.net>
2018-03-12 06:20:22 +0000

    Remove unused module
    Change-Id: I302aefcb5cac50ed08498922c417278656dbdbdd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,9 +16,7 @@
 # limitations under the License.
 
 import os
-import subprocess
 import sys
-import warnings
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
