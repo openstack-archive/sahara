45714289e7530089bb3e08f6cfbc9a469f271362
Luigi Toscano <ltoscano@redhat.com>
2018-09-18 11:49:26 +0200

    grenade: relevant fixes for master (sahara-api/apache)
    - include lib/apache when appropriate;
    - use devstack@sahara-eng for sahara-engine;
    - do not check for sahara-api, as the stop/start_sahara code should
      take care of it (both for the old case and the wsgi case).
    
    Change-Id: I22bb6d51551b6c2540de09e7f307cef27aba0f23

--- devstack/upgrade/shutdown.sh
+++ devstack/upgrade/shutdown.sh
@@ -12,6 +12,7 @@ set -o errexit
 . $BASE_DEVSTACK_DIR/stackrc # needed for status directory
 
 . $BASE_DEVSTACK_DIR/lib/tls
+. $BASE_DEVSTACK_DIR/lib/apache
 . ${GITDIR[sahara]}/devstack/plugin.sh
 
 set -o xtrace
@@ -20,4 +21,4 @@ export ENABLED_SERVICES+=,sahara-api,sahara-eng,
 stop_sahara
 
 # sanity check that service is actually down
-ensure_services_stopped sahara-api sahara-eng
+ensure_services_stopped sahara-eng
