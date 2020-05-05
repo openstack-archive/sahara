a6ee52234382f8c52023796ed13cb3cdc8521098
Corey Bryant <corey.bryant@canonical.com>
2020-05-05 15:52:10 -0400

    Monkey patch original current_thread _active
    Monkey patch the original current_thread to use the up-to-date _active
    global variable. This solution is based on that documented at:
    https://github.com/eventlet/eventlet/issues/592
    
    Change-Id: I93ce7f11dcd8c1f01616580b65e5e581b775e8c5
    Story: 2007614
    (cherry picked from commit 9c83e69e70acf08ced79d409ae73ff2cc756216c)

--- sahara/utils/patches.py
+++ sahara/utils/patches.py
@@ -40,6 +40,12 @@ def eventlet_monkey_patch():
     monkey_patch multiple times.
     """
     eventlet.monkey_patch(**EVENTLET_MONKEY_PATCH_MODULES)
+    # Monkey patch the original current_thread to use the up-to-date _active
+    # global variable. See https://bugs.launchpad.net/bugs/1863021 and
+    # https://github.com/eventlet/eventlet/issues/592
+    import __original_module_threading as orig_threading
+    import threading  # noqa
+    orig_threading.current_thread.__globals__['_active'] = threading._active
 
 
 def eventlet_import_monkey_patched(module):
